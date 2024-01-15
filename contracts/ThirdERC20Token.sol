e6e226a28c3f46aeb94c903fae3bb12621a01868
stevewilli@m <steve980102@gmail.com>
2024-01-16 00:42:25 +0900

    totalSupply revise completely

--- contracts/ThirdERC20Token.sol
+++ contracts/ThirdERC20Token.sol
@@ -6,27 +6,21 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract ThirdERC20Token is ERC20, ERC20Burnable, Ownable {
-    uint256 private _totalSupply = 10000000;
+    uint256 private _initialSupply = 10_000_000;
 
     constructor() ERC20("ThirdERC20Token", "TET") {
-        _mint(msg.sender, _totalSupply * (10 ** decimals()));
+        _mint(msg.sender, _initialSupply * (10 ** decimals()));
     }
 
     function mint(address account, uint256 amount) public onlyOwner {
         _mint(account, amount);
-        _totalSupply += amount;
     }
 
     function burn(address account, uint256 amount) public onlyOwner {
         _burn(account, amount);
-        _totalSupply -= amount;
     }
 
     function decimals() public view virtual override returns (uint8) {
         return 6;
-    }
-
-    function totalSupply() public view virtual override returns (uint256) {
-        return _totalSupply;
     }
 }
