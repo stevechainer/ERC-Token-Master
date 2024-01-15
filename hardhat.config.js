93da5feda9101ef889c76f81dfe3d772eaa11f80
stevewilli@m <steve980102@gmail.com>
2024-01-15 23:42:11 +0900

    dotenv module adding && configure goerli testnet.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+require("dotenv").config();
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: "0.8.20",
+  networks: {
+    goerli: {
+      // Ankr's Public RPC URL
+      url: process.env.INFURA_GOERLI_ENDPOINT,
+      // PRIVATE_KEY loaded from .env file
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
+  },
 };
